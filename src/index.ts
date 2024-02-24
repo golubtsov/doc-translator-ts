047aa782b92e3dfcf9fb678f14d16a8a741268e4
nikita <nproger781@gmail.com>
2024-02-24 17:25:38 +0700

    fix main file

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import LibreTranslator from "./Translator/LibreTranslator.js";
+import Fb2ParallelDocumentGenerator from "./Document/Fb2ParallelDocumentGenerator.js";
+import TranslatorAbstract from "./Translator/TranslatorAbstract.js";
+import DocumentGenerator from "./Document/DocumentGenerator.js";
+
+export default {
+  LibreTranslator,
+  TranslatorAbstract,
+  Fb2ParallelDocumentGenerator,
+  DocumentGenerator,
+};
